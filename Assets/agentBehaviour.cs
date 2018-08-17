ef2d1653d87502ed717f173a0b2fce7fab5bfa68
Guilherme de Quevedo Brendel <guilhermebrendel@owlbytes.com>
2018-08-16 21:28:11 -0300

    Car Model pt1

---
+++ Assets/agentBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class agentBehaviour : MonoBehaviour {
+
+    public Transform destinationTarget;
+
+	// Use this for initialization
+	void Start () {
+
+        lookToPosition(destinationTarget);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    }
+
+    void lookToPosition(Transform destination)
+    {
+        /*
+         Thanks Mike for help with the solution at: https://forum.unity.com/threads/implementing-a-manual-lookat.120308/
+         */
+
+             Vector3 viewForward = Vector3.zero;
+            Vector3 viewUp = Vector3.zero;
+            Vector3 viewRight = Vector3.zero;
+
+
+            // Create viewVector
+            viewForward = destination.position - transform.position;
+
+            // normalize viewVector
+            viewForward.Normalize();
+
+
+
+            // Now we get the perpendicular projection of the viewForward vector onto the world up vector
+            // Uperp = U - ( U.V / V.V ) * V
+            viewUp = Vector3.up - (Vector3.Project(viewForward, Vector3.up));
+            viewUp.Normalize();
+
+            // Alternatively for getting viewUp you could just use:
+            // viewUp = thisTransform.TransformDirection(thisTransform.up);
+            // viewUp.Normalize();
+
+
+            // Calculate rightVector using Cross Product of viewOut and viewUp
+            // this is order is because we use left-handed coordinates
+            viewRight = Vector3.Cross(viewUp, viewForward);
+
+
+            // set new vectors
+            transform.right = new Vector3(viewRight.x, viewRight.y, viewRight.z);
+            transform.up = new Vector3(viewUp.x, viewUp.y, viewUp.z);
+            transform.forward = new Vector3(viewForward.x, viewForward.y, viewForward.z);
+      
+    }
+}
