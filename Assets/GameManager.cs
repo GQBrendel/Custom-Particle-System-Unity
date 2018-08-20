6b2e00fc7196822e96c39a59660ee6ded424c94d
Guilherme Brendel <guilherme.brendel@owlbytes.com>
2018-08-19 23:42:11 -0300

    Finished implementation of the four movements

---
+++ Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    int trackPointIds;
+    public bool shouldStartAllTracks;
+    public List<GameObject> tracks;
+
+    void Start()
+    {
+        trackPointIds = 0;
+
+    }
+    public void setTrackId(idKeeper trackPoint)
+    {
+        trackPoint.id = trackPointIds;
+        trackPointIds++;
+    }
+}
---
+++ Assets/Scripts/lineSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineSpawner : MonoBehaviour {
+
+    public int howFar;
+    int numberOfPoints;
+    public GameObject circlePointPrefab;
+
+
+    void Awake()
+    {
+        circlePointPrefab.name = "track0";        
+        Vector3 pos = new Vector3(transform.position.x + howFar, transform.position.y, transform.position.z);
+        GameObject instance =
+        Instantiate(circlePointPrefab, transform.position + pos, Quaternion.identity) as GameObject;
+        instance.transform.parent = transform;
+        instance.GetComponent<idKeeper>().id = 0;
+
+        circlePointPrefab.name = "track1";
+       // pos = new Vector3(transform.position.x + howFar, transform.position.y, transform.position.z);
+        instance =
+        Instantiate(circlePointPrefab, transform.position, Quaternion.identity) as GameObject;
+        instance.transform.parent = transform;
+        instance.GetComponent<idKeeper>().id = 1;
+    }
+}
